ea9f985e40d1b48d64ec32b1afc66b0843492e06
Nathan <NathHorrigan@gmail.com>
2019-07-02 16:34:09 +0100

    Update realtime admin hook timing

--- grapple/static/js/realtime_preview.js
+++ grapple/static/js/realtime_preview.js
@@ -5,9 +5,10 @@ $(document).ready(() => {
     // Trigger preview save on key up
     let $previewButton = $('.action-preview'), $form = $('#page-edit-form')
     let previewUrl = $previewButton.data('action')
+    let triggerPreviewDataTimeout = -1
     let autoUpdatePreviewDataTimeout = -1
 
-    const setPreviewData = () => {
+    const triggerPreviewUpdate = () => {
         return $.ajax({
             url: `${previewUrl}?realtime_preview=true`,
             method: 'GET',
@@ -17,12 +18,27 @@ $(document).ready(() => {
         })
     }
 
+    const setPreviewData = () => {
+        return $.ajax({
+            url: previewUrl,
+            method: 'POST',
+            data: new FormData($form[0]),
+            processData: false,
+            contentType: false
+        });
+    }
+    
     $previewButton.one('click', function () {
         if ($previewButton.data('auto-update')) {
-            $form.on('change keyup DOMSubtreeModified', function () {
-                clearTimeout(autoUpdatePreviewDataTimeout)
-                autoUpdatePreviewDataTimeout = setTimeout(setPreviewData, 1100)
+            $form.on('click change keyup DOMSubtreeModified', function () {
+                clearTimeout(triggerPreviewDataTimeout)
+                triggerPreviewDataTimeout = setTimeout(triggerPreviewUpdate, 500)
             }).trigger('change')
+
+            $form.on('click change keyup DOMSubtreeModified', function () {
+                clearTimeout(autoUpdatePreviewDataTimeout);
+                autoUpdatePreviewDataTimeout = setTimeout(setPreviewData, 300);
+            }).trigger('change');
         }
     })
 })

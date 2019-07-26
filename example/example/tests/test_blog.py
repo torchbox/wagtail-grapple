ef3ab94109540096b15cfd024b74667372cd24a7
Cameron Lamb <cameron.lamb@torchbox.com>
2019-07-26 17:40:46 +0100

    Add tests for all block types (datetime is failing)

--- example/example/tests/test_blog.py
+++ example/example/tests/test_blog.py
@@ -1,10 +1,12 @@
 import datetime
 import decimal
 
+from django.conf import settings
+
 import wagtail_factories
-from home.blocks import ImageGalleryImages, ImageGalleryImage
+from home.blocks import ImageGalleryImage, ImageGalleryImages
 from home.factories import BlogPageFactory, ImageGalleryImageFactory
-from wagtail.core.blocks import StreamValue, StructValue, BoundBlock
+from wagtail.core.blocks import BoundBlock, StreamValue, StructValue
 from wagtail.core.rich_text import RichText
 
 from .test_grapple import BaseGrappleTest
@@ -48,43 +50,167 @@ class BlogTest(BaseGrappleTest):
         query = '''
         {
             page(id:%s) {
-                title
+                ... on BlogPage {
+                    title
+                }
+            }
+        }
+        ''' % (self.blog_page.id)
+        executed = self.client.execute(query)
+
+        # Check title.
+        self.assertEquals(
+            executed['data']['page']['title'],
+            self.blog_page.title,
+        )
+
+    def get_blocks_from_body(self, block_type, block_query='rawValue'):
+        query = '''
+        {
+            page(id:%s) {
                 ... on BlogPage {
                     body {
-                        id
                         blockType
-                        ... on CharBlock {
-                            rawValue
-                        }
-                        ... on RichTextBlock {
-                            rawValue
-                        }
-                        ... on ImageChooserBlock {
-                            image {
-                                id
-                                src
-                            }
-                        }
-                        ... on DecimalBlock {
-                            rawValue
-                        }
-                        ... on DateBlock {
-                            rawValue
-                        }
-                        ... on DateTimeBlock {
-                            rawValue
-                        }
-                        ... on ImageGalleryBlock {
-                            value
+                        ... on %s {
+                            %s
                         }
                     }
                 }
             }
         }
-        ''' % (self.blog_page.id)
-
-
+        ''' % (self.blog_page.id, block_type, block_query)
         executed = self.client.execute(query)
 
-        from pprint import pprint
-        pprint(executed)
+        blocks = []
+        for block in executed['data']['page']['body']:
+            if block['blockType'] == block_type:
+                blocks.append(block)
+        return blocks
+
+    def test_blog_body_charblock(self):
+        block_type = 'CharBlock'
+        query_blocks = self.get_blocks_from_body(block_type)
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(query_blocks[count]['rawValue'], block.value)
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)
+
+    def test_blog_body_richtextblock(self):
+        block_type = 'RichTextBlock'
+        query_blocks = self.get_blocks_from_body(block_type)
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(query_blocks[count]['rawValue'], block.value.__html__())
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)
+
+    def test_blog_body_imagechooserblock(self):
+        block_type = 'ImageChooserBlock'
+        query_blocks = self.get_blocks_from_body(
+            block_type,
+            block_query='''
+            image {
+                id
+                src
+            }
+            ''',
+        )
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(
+                    query_blocks[count]['image']['id'],
+                    str(block.value.id),
+                )
+                self.assertEquals(
+                    query_blocks[count]['image']['src'],
+                    settings.BASE_URL + block.value.file.url,
+                )
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)
+
+    def test_blog_body_decimalblock(self):
+        block_type = 'DecimalBlock'
+        query_blocks = self.get_blocks_from_body(block_type)
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(query_blocks[count]['rawValue'], str(block.value))
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)
+
+    def test_blog_body_dateblock(self):
+        block_type = 'DateBlock'
+        query_blocks = self.get_blocks_from_body(block_type)
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(query_blocks[count]['rawValue'], str(block.value))
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)
+
+    def test_blog_body_datetimeblock(self):
+        block_type = 'DateTimeBlock'
+        query_blocks = self.get_blocks_from_body(block_type)
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(query_blocks[count]['rawValue'], str(block.value))
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)
+
+    def test_blog_body_imagegalleryblock(self):
+        block_type = 'ImageGalleryBlock'
+        query_blocks = self.get_blocks_from_body(
+            block_type,
+            block_query='value',
+        )
+    
+        # Check output.
+        count = 0
+        for block in self.blog_page.body:
+            if type(block.block).__name__ == block_type:
+                # Test the values
+                self.assertEquals(query_blocks[count]['value']['title'], str(block.value['title']))
+                for key, image in enumerate(query_blocks[count]['value']['images']):
+                    self.assertEquals(
+                        image['image'],
+                        block.value['images'][key].value['image'].id,
+                    )
+                # Increment the count
+                count += 1
+        # Check that we test all blocks that were returned.
+        self.assertEquals(len(query_blocks), count)

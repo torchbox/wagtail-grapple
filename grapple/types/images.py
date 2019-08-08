2dd9f522c5f901b6c7f7f57af433f8bccbd3d9f4
Nathan <NathHorrigan@gmail.com>
2019-08-08 19:26:31 +0100

    remove gatsby specific image code

--- grapple/types/images.py
+++ grapple/types/images.py
@@ -11,16 +11,42 @@ from wagtail.images.models import (
 )
 
 from ..registry import registry
-from ..utils import (
-    convert_image_to_bmp,
-    trace_bitmap,
-    image_as_base64,
-    resolve_queryset,
-)
+from ..utils import resolve_queryset
 from .structures import QuerySetList
 
 
-class ImageRenditionObjectType(DjangoObjectType):
+def get_image_url(cls):
+    if hasattr(cls, 'url'):
+        return cls.url
+
+    return cls.file.url
+
+class BaseImageObjectType(graphene.ObjectType):
+    src = graphene.String()
+    aspect_ratio = graphene.Float()
+    sizes = graphene.String()
+
+    def resolve_src(self, info):
+        """
+        Get url of the original uploaded image.
+        """
+        url = get_image_url(self)
+
+        if (url[0] == '/'):
+            return settings.BASE_URL + url
+        return url
+
+    def resolve_aspect_ratio(self, info, **kwargs):
+        """
+        Calculate aspect ratio for the image.
+        """
+        return self.width / self.height
+
+    def resolve_sizes(self, info):
+        return "(max-width: {}px) 100vw, {}px".format(self.width, self.width)
+
+
+class ImageRenditionObjectType(DjangoObjectType, BaseImageObjectType):
     id = graphene.ID()
     url = graphene.String()
     width = graphene.Int()
@@ -29,10 +55,11 @@ class ImageRenditionObjectType(DjangoObjectType):
     class Meta:
         model = WagtailImageRendition
 
+    def resolve_image(self, info, **kwargs):
+        return self.image
 
-class ImageObjectType(DjangoObjectType):
-    src = graphene.String()
-    src_set = graphene.String(sizes=graphene.List(graphene.Int))
+
+class ImageObjectType(DjangoObjectType, BaseImageObjectType):
     rendition = graphene.Field(
         ImageRenditionObjectType,
         max=graphene.String(),
@@ -44,10 +71,8 @@ class ImageObjectType(DjangoObjectType):
         bgcolor=graphene.String(),
         jpegquality=graphene.Int(),
     )
-    traced_SVG = graphene.String(name="tracedSVG")
-    base64 = graphene.String()
-    aspect_ratio = graphene.Float()
-    sizes = graphene.String()
+    src_set = graphene.String(sizes=graphene.List(graphene.Int))
+
 
     class Meta:
         model = WagtailImage
@@ -59,17 +84,15 @@ class ImageObjectType(DjangoObjectType):
         """
         filters = "|".join([f"{key}-{val}" for key, val in kwargs.items()])
         img = self.get_rendition(filters)
-        return ImageRenditionObjectType(
-            id=img.id, url=img.url, width=img.width, height=img.height
-        )
 
-    def resolve_src(self, info):
-        """
-        Get url of the original uploaded image.
-        """
-        if self.file.url[0] == "/":
-            return settings.BASE_URL + self.file.url
-        return self.file.url
+        return ImageRenditionObjectType(
+            id=img.id, 
+            url=img.url, 
+            width=img.width, 
+            height=img.height,
+            file=img.file,
+            image=self
+        )
 
     def resolve_src_set(self, info, sizes, **kwargs):
         """
@@ -83,42 +106,6 @@ class ImageObjectType(DjangoObjectType):
         return ", ".join(
             [f"{settings.BASE_URL + img.url} {img.width}w" for img in rendition_list]
         )
-
-    def resolve_base64(self, info):
-        """
-        Intended to be used by Gatsby Image. Return the image as base-encoded string so that it can be pre-rendered
-        as background image while actual image is downloaded via network.
-        """
-        return image_as_base64(self.file.url)
-
-    def resolve_traced_SVG(self, info):
-        """
-        Intended to be used by Gatsby Image. Trace a bitmap image and return a small-sized svg that can be pre-rendered
-        as background image while actual image is downloaded via network.
-        """
-        svg_trace_image = (
-            settings.BASE_DIR + os.path.splitext(self.file.url)[0] + "-traced.svg"
-        )
-        if not os.path.isfile(svg_trace_image):
-            temp_image = convert_image_to_bmp(self.file.path)
-            trace_bitmap(temp_image, svg_trace_image)
-
-        with codecs.open(
-            svg_trace_image, "r", encoding="utf-8", errors="ignore"
-        ) as svgFile:
-            file = "data:image/svg+xml," + urllib.parse.quote(svgFile.read())
-
-        return file
-
-    def resolve_aspect_ratio(self, info, **kwargs):
-        """
-        Calculate aspect ratio for Gatsby Image.
-        """
-        return self.width / self.height
-
-    def resolve_sizes(self, info):
-        return "(max-width: {}px) 100vw, {}px".format(self.width, self.width)
-
 
 def ImagesQuery():
     from wagtail.images import get_image_model
@@ -135,7 +122,6 @@ def ImagesQuery():
             return resolve_queryset(mdl.objects.all(), info, **kwargs)
 
     return Mixin
-
 
 def get_image_type():
     from wagtail.images import get_image_model

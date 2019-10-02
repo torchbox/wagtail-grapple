b4de35d0f3a0d3a5277d3f884c981eb8275c4be2
Nathan <NathHorrigan@gmail.com>
2019-10-02 16:38:21 +0100

    0.2.0 Release

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2019, Nathan Horrigan"
 author = "Nathan Horrigan"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------

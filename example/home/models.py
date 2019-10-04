4bc4072c681ee5298b4975fb54e2f34427f882fd
Nathan <NathHorrigan@gmail.com>
2019-10-04 18:14:07 +0100

    implement `register_query_field`

--- example/home/models.py
+++ example/home/models.py
@@ -19,6 +19,7 @@ from wagtail.documents.edit_handlers import DocumentChooserPanel
 from wagtail_headless_preview.models import HeadlessPreviewMixin
 from wagtailmedia.edit_handlers import MediaChooserPanel
 
+from grapple.helpers import register_query_field
 from grapple.models import (
     GraphQLField,
     GraphQLString,
@@ -159,6 +160,7 @@ class Author(Orderable):
 
 
 @register_snippet
+@register_query_field('advert')
 class Advert(models.Model):
     url = models.URLField(null=True, blank=True)
     text = models.CharField(max_length=255)
--- grapple/helpers.py
+++ grapple/helpers.py
@@ -2,6 +2,9 @@ import graphene
 import inspect
 from typing import Type
 from types import MethodType
+
+from wagtail.core.models import Page
+
 from graphene_django.types import DjangoObjectType
 
 from .registry import registry
@@ -34,3 +37,57 @@ def register_streamfield_block(cls):
 def register_graphql_schema(schema_cls):
     registry.schema.append(schema_cls)
     return schema_cls
+
+
+def register_query_field(field_name, plural_field_name=None, query_params=None):
+    from .types.structures import QuerySetList
+    from .utils import resolve_queryset
+
+    if not plural_field_name:
+        plural_field_name = field_name + "s"
+
+    def inner(cls):
+        field_type = lambda: registry.models[cls]
+        field_query_params = query_params or {"id": graphene.Int()}
+
+        def Mixin():
+            # Generic methods to get all and query one model instance.
+            def resolve_singular(self, _, info, **kwargs):
+                # If no filters then return nothing,
+                if not kwargs:
+                    return None
+
+                # If is a Page then only query live/public pages.
+                if issubclass(cls, Page):
+                    return cls.objects.live().public().get(**kwargs)
+
+                return cls.objects.get(**kwargs)
+
+            def resolve_plural(self, _, info, **kwargs):
+                qs = cls.objects
+                if issubclass(cls, Page):
+                    qs = cls.objects.live().public()
+
+                return resolve_queryset(qs.all(), info, **kwargs)
+
+            # Create schema and add resolve methods
+            schema = type(cls.__name__ + "Query", (), {})
+            setattr(
+                schema, field_name, graphene.Field(field_type, **field_query_params)
+            )
+            setattr(schema, plural_field_name, QuerySetList(field_type))
+            setattr(
+                schema, "resolve_" + field_name, MethodType(resolve_singular, schema)
+            )
+            setattr(
+                schema,
+                "resolve_" + plural_field_name,
+                MethodType(resolve_plural, schema),
+            )
+            return schema
+
+        # Send schema to Grapple schema.
+        register_graphql_schema(Mixin())
+        return cls
+
+    return inner

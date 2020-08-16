fe3a2fd160cc95b6e42afa6af55af5222e1eb12b
Fabien Le Frapper <contact@fabienlefrapper.me>
2020-08-16 15:46:02 +0200

    Add tags to images

--- grapple/types/images.py
+++ grapple/types/images.py
@@ -73,6 +73,7 @@ class ImageObjectType(DjangoObjectType, BaseImageObjectType):
         webpquality=graphene.Int(),
     )
     src_set = graphene.String(sizes=graphene.List(graphene.Int))
+    tags = graphene.List(graphene.String))
 
     class Meta:
         model = WagtailImage
@@ -119,6 +120,16 @@ class ImageObjectType(DjangoObjectType, BaseImageObjectType):
             pass
 
         return ""
+
+    def resolve_tags(self, info, **kwargs):
+        """
+        Generate list of tags
+        """
+        try:
+            if self.tags.all().count > 0:
+                self.tags.all().join(", ")
+        except:
+            return list()
 
 
 def get_image_type():

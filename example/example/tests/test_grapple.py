cec4f8dd5cfa55eb47ca6e527cb4f5ea27afbeb9
Tibor <tibor@lpld.io>
2020-08-17 22:03:11 -1000

    Add `url` to `DocumentObjectType` (#95)
    * Add simple test for existing document schema
    
    * Add test for fileHash and fileSize GraphQL fields
    
    * Add assertions to test setup for document test
    
    * Add test for image query
    
    Image queries before have been tested through some page tests, but not
    directly.
    
    The new test is only minimal but could be extended for more image fields
    that can be queried.
    
    The test includes the `src` filed on the image query. This test serves as
    an example to add the same test for the documents next and then implement
    the filed on the document similar to how it exists on the image already.
    
    * Change document test names for clarity
    
    * Add failing test for documents.src field
    
    The test is failing because the field does not exist on the endpoint yet.
    Implementing the field and passing the test is the next step
    
    * Duplicate src query test for different document serve methods
    
    The URL in src will differ depending on the document serve method used.
    
    The tests are now sensitive to this different and expect different values.
    To make the default method explicit, the corresponding option has been added.
    
    * Add src field to DocumentObjectType
    
    The method to resolve the `src` field is basically the same as for the BaseImageObjectType.
    
    * Blackify
    
    * Change field name from src to url in test
    
    This change was requested by @zerolabs in
    https://github.com/GrappleGQL/wagtail-grapple/pull/96\#issuecomment-674950403
    and
    https://github.com/GrappleGQL/wagtail-grapple/issues/94\#issuecomment-674955038
    
    * Update field name from src to url

--- example/example/tests/test_grapple.py
+++ example/example/tests/test_grapple.py
@@ -1,12 +1,19 @@
+import os
+
 from collections import OrderedDict
 from pydoc import locate
 
 from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings
 
 from graphene.test import Client
 
 from wagtail.core.models import Page
+from wagtail.documents import get_document_model
+from wagtail.images import get_image_model
+
+import wagtail_factories
 
 from grapple.schema import create_schema
 
@@ -66,3 +73,181 @@ class DisableAutoCamelCaseTest(TestCase):
         pages = Page.objects.all()
 
         self.assertEquals(len(executed["data"]["pages"]), pages.count())
+
+
+class ImagesTest(BaseGrappleTest):
+    def setUp(self):
+        super().setUp()
+        self.image_model = get_image_model()
+        self.assertEqual(self.image_model.objects.all().count(), 0)
+        self.example_image = wagtail_factories.ImageFactory(title="Example Image")
+        self.example_image.full_clean()
+        self.example_image.save()
+        self.assertEqual(self.image_model.objects.all().count(), 1)
+
+    def test_properties_on_saved_example_image(self):
+        example_img = self.image_model.objects.first()
+
+        self.assertEqual(example_img.id, 1)
+        self.assertEqual(example_img.title, "Example Image")
+
+    def test_query_src_field(self):
+        query = """
+        {
+            images {
+                id
+                src
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        self.assertEquals(executed["data"]["images"][0]["id"], "1")
+        self.assertEquals(
+            executed["data"]["images"][0]["src"],
+            "http://localhost:8000" + self.example_image.file.url,
+        )
+
+    def tearDown(self):
+        example_image_path = self.example_image.file.path
+        self.example_image.delete()
+        os.remove(example_image_path)
+
+
+class DocumentsTest(BaseGrappleTest):
+    def setUp(self):
+        super().setUp()
+        self.document_model = get_document_model()
+        self.assertEqual(self.document_model.objects.all().count(), 0)
+
+        uploaded_file = SimpleUploadedFile("example.txt", b"Hello world!")
+        self.example_document = self.document_model(
+            title="Example File", file=uploaded_file
+        )
+        self.example_document.full_clean()
+        self.example_document.save()
+        self.example_document.get_file_hash()
+        self.example_document.get_file_size()
+        self.assertEqual(self.document_model.objects.all().count(), 1)
+
+    def test_properties_on_saved_example_document(self):
+        example_doc = self.document_model.objects.first()
+
+        self.assertEqual(example_doc.id, 1)
+        self.assertEqual(example_doc.title, "Example File")
+
+        example_doc.file.seek(0)
+
+        self.assertEqual(example_doc.file.readline(), b"Hello world!")
+
+        self.assertNotEqual(example_doc.file_hash, "")
+        self.assertNotEqual(example_doc.file_size, None)
+
+    def test_query_documents_id(self):
+        query = """
+        {
+            documents {
+                id
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        documents = self.document_model.objects.all()
+
+        self.assertEquals(len(executed["data"]["documents"]), documents.count())
+        self.assertEquals(
+            executed["data"]["documents"][0]["id"], str(self.example_document.id)
+        )
+
+    def test_query_file_field(self):
+        query = """
+        {
+            documents {
+                id
+                file
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        self.assertEquals(
+            executed["data"]["documents"][0]["file"], self.example_document.file.name
+        )
+
+    def test_query_file_hash_field(self):
+        query = """
+        {
+            documents {
+                id
+                fileHash
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        self.assertEquals(
+            executed["data"]["documents"][0]["fileHash"],
+            self.example_document.file_hash,
+        )
+
+    def test_query_file_size_field(self):
+        query = """
+        {
+            documents {
+                id
+                fileSize
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        self.assertEquals(
+            executed["data"]["documents"][0]["fileSize"],
+            self.example_document.file_size,
+        )
+
+    def test_query_url_field_with_default_document_serve_method(self):
+        query = """
+        {
+            documents {
+                id
+                url
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        self.assertEqual(
+            executed["data"]["documents"][0]["url"],
+            "http://localhost:8000" + self.example_document.url,
+        )
+
+    def test_query_url_field_with_direct_document_serve_method(self):
+        serve_method_at_test_start = settings.WAGTAILDOCS_SERVE_METHOD
+        settings.WAGTAILDOCS_SERVE_METHOD = "direct"
+        query = """
+        {
+            documents {
+                id
+                url
+            }
+        }
+        """
+
+        executed = self.client.execute(query)
+
+        self.assertEqual(
+            executed["data"]["documents"][0]["url"],
+            "http://localhost:8000" + self.example_document.file.url,
+        )
+        settings.WAGTAILDOCS_SERVE_METHOD = serve_method_at_test_start
+
+    def tearDown(self):
+        self.example_document.file.delete()

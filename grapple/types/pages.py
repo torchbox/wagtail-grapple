40f0247c0241b191fe510f739963f4df0bec198e
Dan Braghis <dan.braghis@torchbox.com>
2020-10-07 12:27:19 +0100

    Fix parent attribute

--- grapple/types/pages.py
+++ grapple/types/pages.py
@@ -80,7 +80,7 @@ class PageInterface(graphene.Interface):
         Docs: https://docs.wagtail.io/en/stable/reference/pages/model_reference.html#wagtail.core.models.Page.get_parent
         """
         try:
-            return resolve_queryset(self.get_parent().specific, info, **kwargs)
+            return self.get_parent().specific
         except GraphQLLocatedError:
             return WagtailPage.objects.none()
 

c3b92f014f1ec8f39bf6c640f0319d7fb7ce6087
Dan Braghis <dan.braghis@torchbox.com>
2021-03-08 11:00:35 +0000

    Update grapple/types/images.py
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>

--- grapple/types/images.py
+++ grapple/types/images.py
@@ -63,7 +63,7 @@ def get_rendition_type():
 def rendition_allowed(rendition_filter):
     """Checks a given rendition filter is allowed"""
     allowed_filters = getattr(settings, "GRAPPLE_ALLOWED_IMAGE_FILTERS", None)
-    if allowed_filters is None or not isinstance(allowed_filters, list):
+    if allowed_filters is None or not isinstance(allowed_filters, (list, tuple)):
         return True
 
     return rendition_filter in allowed_filters

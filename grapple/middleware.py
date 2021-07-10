98e7dc019829637c6795252c2bc7912f93670343
Rui Saraiva <ruisaraiva19@gmail.com>
2021-07-10 21:45:17 +0100

    chore: clean up and improve performance

--- grapple/middleware.py
+++ grapple/middleware.py
@@ -1,10 +1,24 @@
 import inspect
 
+from graphql.execution.middleware import MIDDLEWARE_RESOLVER_FUNCTION
 from graphene import ResolveInfo
 
 from .registry import registry
 
 ROOT_TYPES = ["Query", "Mutation", "Subscription"]
+
+
+def get_middleware_resolvers(middlewares):
+    for middleware in middlewares:
+        if inspect.isfunction(middleware):
+            yield middleware
+        if not hasattr(middleware, MIDDLEWARE_RESOLVER_FUNCTION):
+            raise Exception(
+                "Middleware must be either a class of a function but got: {}.\nYou can read more about middleware here: https://docs.graphene-python.org/en/latest/execution/middleware/".format(
+                    type(middleware)
+                )
+            )
+        yield getattr(middleware(), MIDDLEWARE_RESOLVER_FUNCTION)
 
 
 class IsAuthenticatedMiddleware(object):
@@ -22,20 +36,18 @@ class IsAnonymousMiddleware(object):
 
 
 class GrappleMiddleware(object):
+    def __init__(self):
+        self.field_middlewares = {}
+        for field_name in registry.field_middlewares:
+            self.field_middlewares[field_name] = list(
+                get_middleware_resolvers(registry.field_middlewares[field_name])
+            )
+
     def resolve(self, next, root, info: ResolveInfo, **args):
         field_name = info.field_name
         parent_name = info.parent_type.name
-        if field_name in registry.field_middlewares and parent_name in ROOT_TYPES:
-            for middleware in registry.field_middlewares[field_name]:
-                if inspect.isfunction(middleware):
-                    return middleware(next, root, info, **args)
-                elif inspect.isclass(middleware):
-                    return middleware().resolve(next, root, info, **args)
-                else:
-                    raise Exception(
-                        "Middleware must be either a class of a function but got: {}.\nYou can read more about middleware here: https://docs.graphene-python.org/en/latest/execution/middleware/".format(
-                            type(middleware)
-                        )
-                    )
+        if field_name in self.field_middlewares and parent_name in ROOT_TYPES:
+            for middleware in self.field_middlewares[field_name]:
+                return middleware(next, root, info, **args)
 
         return next(root, info, **args)

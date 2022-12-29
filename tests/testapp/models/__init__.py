4a5a96735f1a8e08e2b01e93411caafc5ed19c06
Dan Braghis <dan.braghis@torchbox.com>
2022-12-29 23:03:56 +0000

    Re-organise test app
    * move all apps into a single testapp
    * move test_*.py under tests/
    * regenerate migrations

---
+++ tests/testapp/models/__init__.py
@@ -0,0 +1,2 @@
+from .core import *  # noqa
+from .media import *  # noqa
---
+++ tests/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+from grapple import urls as grapple_urls
+
+urlpatterns = [
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("", include(grapple_urls)),
+    path("", include(wagtail_urls)),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

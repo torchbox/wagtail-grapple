31a0419f61ac827d0e56c8ad5e250d9b876cc023
Dan Braghis <dan.braghis@torchbox.com>
2023-01-13 17:09:46 +0000

    Fix DEPRECATED_SETTINGS breaks apps

--- example/example/tests/test_settings.py
+++ example/example/tests/test_settings.py
@@ -15,9 +15,7 @@ class TestSettings(TestCase):
         """
         if len(DEPRECATED_SETTINGS) > 0:
             with self.assertLogs("grapple", level="WARNING"):
-                user_settings = {}
-                user_settings[DEPRECATED_SETTINGS[0]] = True
-                GrappleSettings(user_settings)
+                GrappleSettings({DEPRECATED_SETTINGS[0]: True})
 
     def test_error_raised_on_removed_setting(self):
         """
@@ -25,9 +23,7 @@ class TestSettings(TestCase):
         """
         if len(REMOVED_SETTINGS) > 0:
             with self.assertRaises(RuntimeError):
-                user_settings = {}
-                user_settings[REMOVED_SETTINGS[0]] = True
-                GrappleSettings(user_settings)
+                GrappleSettings({REMOVED_SETTINGS[0]: True})
 
     def test_compatibility_with_override_settings(self):
         """

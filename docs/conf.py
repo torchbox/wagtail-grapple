68195c05b20b2321273c332f8ef12a3bf8462df8
zerolab <dan@zerolab.org>
2023-07-10 09:24:13 +0100

    Update docs configuration
    - move dependencies to pyproject.toml
    - update the RTD config
    - update sphinx_wagtail_theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 
@@ -27,45 +15,43 @@ sys.path.insert(0, os.path.abspath("./grapple"))
 project = "Wagtail Grapple"
 copyright = "2019, Nathan Horrigan. 2020-present Dan Braghis and contributors"
 author = "Nathan Horrigan"
-
-# The full version, including alpha/beta/rc tags
 release = __version__
+version = __version__
 
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinx_wagtail_theme"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.spelling",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_wagtail_theme"
 html_theme_path = [sphinx_wagtail_theme.get_html_theme_path()]
 html_theme_options = {
     "project_name": "Wagtail Grapple Documentation",
     "github_url": "https://github.com/torchbox/wagtail-grapple/tree/main/docs/",
+    "logo": "img/wagtail-grapple.svg",
+    "footer_links": "",
 }
+html_last_updated_fmt = "%b %d, %Y"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+pygments_style = None  # covered by sphinx_wagtail_theme
+lexers["graphql"] = GraphqlLexer()
 
 master_doc = "index"
 
+spelling_lang = "en_US"
+spelling_word_list_filename = "spelling_wordlist.txt"
 
-lexers["graphql"] = GraphqlLexer()
+# -- Misc --------------------------------------------------------------------
+
+epub_show_urls = "footnote"

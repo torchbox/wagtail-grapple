3348dcd9f66676f03ac06117840f8582893bcc8a
Jakub Mastalerz <jakub.mastalerz@torchbox.com>
2023-12-20 09:40:47 +0100

    Refined redirects.py

--- grapple/types/redirects.py
+++ grapple/types/redirects.py
@@ -17,30 +17,24 @@ class RedirectObjectType(graphene.ObjectType):
     old_url = graphene.String(required=True)
     new_url = graphene.String(required=False)
     page = graphene.Field(get_page_interface())
-    site = graphene.Field(SiteObjectType, required=False)
+    site = graphene.Field(SiteObjectType, required=True)
     is_permanent = graphene.Boolean(required=True)
 
     def resolve_old_url(self, info, **kwargs) -> str:
         """
         Resolve the value of `old_url` using the `root_url` of the associated
         site and `old_path`.
-        Note: `self.site` should never be none because of `resolve_redirects`.
         """
-        if self.site:
-            return f"{self.site.root_url}/{self.old_path}"
 
-        if self.site is None:
-            return None
+        return f"{self.site.root_url}/{self.old_path}"
 
-    # Get new url
     def resolve_new_url(self, info, **kwargs) -> Optional[str]:
-        if self.redirect_page:
-            return self.redirect_page.url
+        """
+        Resolve the value of `new_url`. If `redirect_page` is specified then it's
+        url is prioritised.
+        """
 
-        if self.link:
-            return self.link
-
-        return None
+        return self.link
 
     # Return the page that's being redirected to, if at all.
     def resolve_page(self, info, **kwargs) -> Optional[Page]:
@@ -55,22 +49,24 @@ class RedirectsQuery:
     redirects = graphene.List(graphene.NonNull(RedirectObjectType), required=True)
 
     # Return all redirects.
-    def resolve_redirects(self, info, **kwargs):
+    def resolve_redirects(self, info, **kwargs) -> list[Redirect]:
         """
         Resolve the query set of redirects. If `site` is None, a redirect works
         for all sites. To show this, a new redirect object is created for each
         of the sites.
         """
-        redirects_QS = Redirect.objects.select_related("redirect_page")
-        redirect_list = list(redirects_QS)
-        sites_QS = Site.objects.all()
-        for redirect in redirect_list:
+
+        redirects_qs = Redirect.objects.select_related("redirect_page").all()
+        finalised_redirects: list[
+            Redirect
+        ] = []  # Redirects to return within API response.
+        for redirect in redirects_qs:
             if redirect.site is None:
-                for site in sites_QS:
-                    new_redirect = copy.copy(redirect)
-                    new_redirect.site = site
-                    redirect_list.append(new_redirect)
-
-        redirect_list = filter(lambda x: (x.site is not None), redirect_list)
-
-        return redirect_list
+                # Duplicate Redirect for each Site as it applies to all Sites.
+                for site in Site.objects.all():
+                    _new_redirect = copy.copy(redirect)
+                    _new_redirect.site = site
+                    finalised_redirects.append(_new_redirect)
+            else:
+                finalised_redirects.append(redirect)
+        return finalised_redirects

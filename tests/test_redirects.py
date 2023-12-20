19fc4ed244cfc89a842bfa0a7b483db805db0ca9
Jakub Mastalerz <jakub.mastalerz@torchbox.com>
2023-12-20 10:09:05 +0100

    Refined test_redirects

--- tests/test_redirects.py
+++ tests/test_redirects.py
@@ -1,6 +1,8 @@
 from test_grapple import BaseGrappleTest
 from testapp.factories import RedirectFactory
 from testapp.models import BlogPage
+from wagtail.contrib.redirects.models import Redirect
+from wagtail.models import Site
 from wagtail_factories import SiteFactory
 
 
@@ -16,7 +18,8 @@ class TestRedirectQueries(BaseGrappleTest):
         Test that Redirect fields can be queried through graphql.
         """
 
-        self.redirect = RedirectFactory(
+        # Create a basic redirect without a specified `redirect_page` or `site`.
+        RedirectFactory(
             old_path="test/old",
             redirect_link="http://localhost:8000/test/new",
             is_permanent=True,
@@ -36,6 +39,7 @@ class TestRedirectQueries(BaseGrappleTest):
 
         result = self.client.execute(query)
 
+        # Verify that the structure of the results is correct.
         self.assertEqual(type(result["data"]), dict)
         self.assertEqual(type(result["data"]["redirects"]), list)
         self.assertEqual(type(result["data"]["redirects"][0]), dict)
@@ -51,7 +55,8 @@ class TestRedirectQueries(BaseGrappleTest):
         Test that `Page` and `Site` fields on Redirects can be queried through graphql.
         """
 
-        self.redirect = RedirectFactory(
+        # Create a redirect with specified `redirect_page` and `site`.
+        RedirectFactory(
             redirect_page=self.page,
             site=SiteFactory(
                 hostname="test-site",
@@ -76,6 +81,7 @@ class TestRedirectQueries(BaseGrappleTest):
 
         result = self.client.execute(query)
 
+        # Verify the structure of the query for `page` and `site`.
         self.assertEqual(type(result["data"]["redirects"][0]), dict)
         self.assertEqual(type(result["data"]["redirects"][0]["page"]), dict)
         self.assertEqual(type(result["data"]["redirects"][0]["site"]), dict)
@@ -94,17 +100,16 @@ class TestRedirectQueries(BaseGrappleTest):
         When just one is provided, use that.
         """
 
-        self.redirect_both_sources = RedirectFactory(
-            redirect_link="/test/incorrect", redirect_page=self.page
-        )
-        self.redirect_just_page = RedirectFactory(
+        # Create a redirect with both `redirect_link` and `redirect_page`.
+        RedirectFactory(redirect_link="/test/incorrect", redirect_page=self.page)
+        # Create a redirect with just `redirect_page`.
+        RedirectFactory(
             redirect_link="",
             is_permanent=True,
             redirect_page=self.page,
         )
-        self.redirect_just_link = RedirectFactory(
-            redirect_link="http://test/just-link", redirect_page=None
-        )
+        # Create a redirect with just `redirect_link`.
+        RedirectFactory(redirect_link="http://test/just-link", redirect_page=None)
 
         query = """
         {
@@ -116,8 +121,14 @@ class TestRedirectQueries(BaseGrappleTest):
 
         result = self.client.execute(query)["data"]["redirects"]
 
+        # Sanity check to ensure only those three redirect objects are in the database.
+        self.assertEqual(Redirect.objects.count(), 3)
+
+        # Redirects with a specified `redirect_page` should return its url.
         self.assertEqual(result[0]["newUrl"], "http://localhost/test-page-url/")
         self.assertEqual(result[1]["newUrl"], "http://localhost/test-page-url/")
+        # Redirects without a specified `redirect_page` should return the
+        # `redirect_link` instead.
         self.assertEqual(result[2]["newUrl"], "http://test/just-link")
 
     def test_no_new_url_query(self):
@@ -126,7 +137,9 @@ class TestRedirectQueries(BaseGrappleTest):
         `redirect_page` and queried. This can be used for producing a HTTP 410
         for any pages/URLs that are no longer in existence.
         """
-        self.redirect = RedirectFactory(
+
+        # Create a redirect with no source for `new_url` generation.
+        RedirectFactory(
             redirect_link="",
             redirect_page=None,
         )
@@ -147,21 +160,27 @@ class TestRedirectQueries(BaseGrappleTest):
         """
         Test that a redirect with a specified site returns the correct `old_url`.
         """
-        self.redirect = RedirectFactory(
+        # Create a redirect with `site` using port 81, which should result in a
+        # standard `old_url` generation.
+        RedirectFactory(
             old_path="old-path",
             site=SiteFactory(
                 hostname="test-site",
                 port=81,
             ),
         )
-        self.redirect = RedirectFactory(
+        # Create a redirect with `site` using port 80, which should be resolved
+        # to "http" without a specified port in `old_url`.
+        RedirectFactory(
             old_path="old-path",
             site=SiteFactory(
                 hostname="test-site-default-port",
                 port=80,
             ),
         )
-        self.redirect = RedirectFactory(
+        # Create a redirect with `site` using port 443, which should be resolved
+        # to "https" without a specified port in `old_url`.
+        RedirectFactory(
             old_path="old-path",
             site=SiteFactory(
                 hostname="test-site-secure",
@@ -179,6 +198,9 @@ class TestRedirectQueries(BaseGrappleTest):
 
         result = self.client.execute(query)["data"]["redirects"]
 
+        # Sanity check to ensure only those three redirect objects are in the database.
+        self.assertEqual(Redirect.objects.count(), 3)
+
         self.assertEqual(result[0]["oldUrl"], "http://test-site:81/old-path")
         self.assertEqual(result[1]["oldUrl"], "http://test-site-default-port/old-path")
         self.assertEqual(result[2]["oldUrl"], "https://test-site-secure/old-path")
@@ -188,10 +210,13 @@ class TestRedirectQueries(BaseGrappleTest):
         Test that when no site is specified on a redirect, that redirect is
         shown for each existing site.
         """
-        self.site1 = SiteFactory(hostname="test-site", port=81)
-        self.site2 = SiteFactory(hostname="another-test-site", port=82)
 
-        self.redirect = RedirectFactory(
+        # Create two additional sites (on top of the default `Localhost` one).
+        SiteFactory(hostname="test-site", port=81)
+        SiteFactory(hostname="another-test-site", port=82)
+
+        # Create a `Redirect` without a `site` (treated as active for all sites).
+        RedirectFactory(
             old_path="old-path",
             site=None,
         )
@@ -205,6 +230,16 @@ class TestRedirectQueries(BaseGrappleTest):
         """
 
         result = self.client.execute(query)["data"]["redirects"]
+
+        # Sanity check to ensure there are three `Site`s.
+        self.assertEqual(Site.objects.count(), 3)
+
+        # Sanity check to ensure only one `Redirect` is in the database.
+        self.assertEqual(Redirect.objects.count(), 1)
+
+        # Ensure there are three `Redirect`s returned despite only 1 object
+        # in the database.
+        self.assertEqual(len(result), 3)
 
         self.assertEqual(result[0]["oldUrl"], "http://another-test-site:82/old-path")
         self.assertEqual(result[1]["oldUrl"], "http://localhost/old-path")
